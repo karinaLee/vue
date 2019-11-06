58b8a36d4f8a18edf29d4a2a3279ace3f6be6bf2
katrinaLee <yg12033@naver.com>
2019-11-06 23:09:42 +0900

    완료된일 삭제 기능 추가

--- src/components/taskFooter.vue
+++ src/components/taskFooter.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>Footer</div>
-
+    <v-btn depressed large color="error" @click="deleteTasks">완료된 할 일 모두 삭제</v-btn>
 </template>
 
 <script>
 export default {
-
+    methods : {
+        deleteTasks(){
+            this.$store.commit('TASK/DELETE_TASKS');
+        }
+    }
 }
 </script> 
 
