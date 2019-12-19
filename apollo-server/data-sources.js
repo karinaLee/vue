09306b7ec9b87f44addc761699fb673fb8859cf8
katrinaLee <yg12033@naver.com>
2019-12-19 22:15:49 +0900

    add mutations

--- apollo-server/data-sources.js
+++ apollo-server/data-sources.js
@@ -1,7 +1,10 @@
 const QuakeAPI = require('./datasources/quake');
+ const TaskAPI = require('./datasources/task');
+
 
 export default function() {
   return {
-    quakeAPI : new QuakeAPI()
+    quakeAPI : new QuakeAPI(),
+    taskAPI : new TaskAPI()
   };
 }
--- apollo-server/resolvers.js
+++ apollo-server/resolvers.js
@@ -1,10 +1,28 @@
 import GraphQLJSON from 'graphql-type-json'
-import shortid from 'shortid'
 
+const todos = [
+  {
+    task : 'study vue',
+    completed : false
+  },
+  {
+    task : 'study apollo',
+    completed : false
+  }  
+]
 
 export default {
   JSON: GraphQLJSON,
   Query: {
-    quakes : (_,__,{dataSources}) => dataSources.quakeAPI.getAllQuakes()
+    quakes : (_,__,{dataSources}) => dataSources.quakeAPI.getAllQuakes(),
+    tasks :  (_,__,{dataSources}) => dataSources.taskAPI.getTasks(),
+    todos :  () => todos 
+  },
+  Mutation : {
+    addTodo : (_,{task,completed}) =>{
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;  
+    }
   }
 }
